418c87b54d49486be2fe64f37b4e663cfd74fbfa
LiveEmily <d.claassen2003@outlook.com>
2022-03-02 10:37:10 +0100

    Finish version 2

--- stage1.sh
+++ stage1.sh
@@ -67,10 +67,10 @@ fi
 # Install wget, set time and download other stages
 
 pacman -Sy --noconfirm wget
-#timedatectl set-ntp true
-#wget https://raw.githubusercontent.com/LiveEmily/ArchieInstall/v2.0/stage2.sh
-#wget https://raw.githubusercontent.com/LiveEmily/ArchieInstall/v2.0/pacman.txt
-#wget https://raw.githubusercontent.com/LiveEmily/ArchieInstall/v2.0/yay.txt
+timedatectl set-ntp true
+wget https://raw.githubusercontent.com/LiveEmily/ArchieInstall/v2.0/stage2.sh
+wget https://raw.githubusercontent.com/LiveEmily/ArchieInstall/v2.0/pacman.txt
+wget https://raw.githubusercontent.com/LiveEmily/ArchieInstall/v2.0/aur.txt
 
 
 # Choose a disk
@@ -90,3 +90,178 @@ do
     fi
     echo $diskpart
 done
+
+
+# Optional settings
+
+"$dialog" --title "SWAP space" --yesno "Would you like to have SWAP space?" 8 78 3>&1 1>&2 2>&3
+if [ $? -eq 0 ]; then
+    swap=true
+fi
+    
+"$dialog" --title "Home directory" --yesno "Would you like a seperate partition for /home?" 8 78 3>&1 1>&2 2>&3
+if [ $? -eq 0 ]; then
+    home=true
+fi
+
+if [ $swap == true ]; then
+    while :
+    do
+        swapSize=$("$dialog" --title "Choose swap size" --inputbox "Please choose the amount of swap space you want in GB(GigaBytes).\nUsually it would be best practice to have double your RAM as swap. So 2x$ramgb in your case." 8 78 3>&1 1>&2 2>&3)
+        if [[ $swapSize -gt 0 ]]; then
+            break
+        else
+            "$dialog" --title "Not a valid size!" --msgbox "Please choose a valid size for the swap partition" 20 60 3>&1 1>&2 2>&3
+        fi
+    done
+fi
+
+if [ $home == true ]; then
+    while :
+    do
+        homeSize=$("$dialog" --title "Choose home size" --inputbox "Please choose how big you want your home partition to be in GB(GigaBytes)." 8 78 3>&1 1>&2 2>&3)
+        if [[ $homeSize -gt 0 ]]; then
+            break
+        else
+            "$dialog" --title "Not a valid size!" --msgbox "Please choose a valid size for the home partition" 20 60 3>&1 1>&2 2>&3
+        fi
+    done
+fi
+
+
+# Partitioning the actual disks (This is a huge pain and I'd love to make it simpler)
+
+"$dialog" --title "Partitioning drive" --msgbox "Your disk will now be partitioned, please stand by..." 8 78
+if [[ -d "/sys/firmware/efi" ]]; then
+	if [ $swap == "true" ] && [ $home == "true" ]; then
+		echo ",1048576,6
+		,$(($swapSize*2097152)),S
+		,$(($homeSize*2097152)),83
+		;" | sfdisk /dev/$disk
+		mkswap /dev/"$diskpart"2
+		swapon /dev/"$diskpart"2
+		mkfs.ext4 /dev/"$diskpart"3
+		mkfs.ext4 /dev/"$diskpart"4
+	elif [ $swap == "true" ] && [ $home == "false" ]; then
+		echo ",1048576,6
+		,$(($swapSize*2097152)),S
+		;" | sfdisk /dev/$disk
+		mkswap /dev/"$diskpart"2
+		swapon /dev/"$diskpart"2
+		mkfs.ext4 /dev/"$diskpart"3
+	elif [ $swap == "false" ] && [ $home == "true" ]; then
+		echo ",1048576,6
+		,$(($homeSize*2097152)),83
+		;" | sfdisk /dev/$disk
+		mkfs.ext4 /dev/"$diskpart"2
+		mkfs.ext4 /dev/"$diskpart"3
+	else
+		echo ",1048576,6
+		;" | sfdisk /dev/$disk
+		mkfs.ext4 /dev/"$diskpart"2
+	fi
+	mkfs.vfat -F32 /dev/"$diskpart"1
+else
+	if [ $swap == "true" ] && [ $home == "true" ]; then
+		echo ",2048,a
+		,1048576,6
+		,$(($swapSize*2097152)),S
+		,,E
+		,$(($homeSize*2097152)),83
+		;" | sfdisk /dev/$disk
+		mkswap /dev/"$diskpart"3
+		swapon /dev/"$diskpart"3
+		mkfs.ext4 /dev/"$diskpart"5
+		mkfs.ext4 /dev/"$diskpart"6
+	elif [ $swap == "true" ] && [ $home == "false" ]; then
+		echo ",2048,a
+		,1048576,6
+		,$(($swapSize*2097152)),S
+		;" | sfdisk /dev/$disk
+		mkswap /dev/"$diskpart"3
+		swapon /dev/"$diskpart"3
+		mkfs.ext4 /dev/"$diskpart"4
+	elif [ $swap == "false" ] && [ $home == "true" ]; then
+		echo ",2048,a
+		,1048576,6
+		,$(($homeSize*2097152)),83
+		;" | sfdisk /dev/$disk
+		mkfs.ext4 /dev/"$diskpart"3
+		mkfs.ext4 /dev/"$diskpart"4
+	else
+		echo ",2048,a
+		,1048576,6
+		;" | sfdisk /dev/$disk
+		mkfs.ext4 /dev/"$diskpart"3
+	fi
+	mkfs.vfat -F32 /dev/"$diskpart"2
+fi
+
+
+# Mounting the partitions (Again this is quite painful but bare with me)
+
+"$dialog" --title "Mounting partitions" --msgbox "We will now mount the partitions, please stand by..." 8 78
+if [[ -d "/sys/firmware/efi" ]]; then
+	if [ $swap == "true" ] && [ $home == "true" ]; then
+		mount /dev/"$diskpart"4 /mnt
+		mkdir /mnt/boot
+		mkdir /mnt/home
+		mount /dev/"$diskpart"1 /mnt/boot
+		mount /dev/"$diskpart"3 /mnt/home
+	elif [ $swap == "false" ] && [ $home == "true" ]; then
+		mount /dev/"$diskpart"3 /mnt
+		mkdir /mnt/boot
+		mkdir /mnt/home
+		mount /dev/"$diskpart"1 /mnt/boot
+		mount /dev/"$diskpart"2 /mnt/home
+	elif [ $swap == "true" ] && [ $home == "false" ]; then
+		mount /dev/"$diskpart"3 /mnt
+		mkdir /mnt/boot
+		mount /dev/"$diskpart"1 /mnt/boot
+	else
+		mount /dev/"$diskpart"2 /mnt
+		mkdir /mnt/boot
+		mount /dev/"$diskpart"1 /mnt/boot
+	fi
+else
+	if [ $swap == "true" ] && [ $home == "true" ]; then
+		mount /dev/"$diskpart"6 /mnt
+		mkdir /mnt/boot
+		mkdir /mnt/home
+		mount /dev/"$diskpart"2 /mnt/boot
+		mount /dev/"$diskpart"5 /mnt/home
+	elif [ $swap == "false" ] && [ $home == "true" ]; then
+		mount /dev/"$diskpart"4 /mnt
+		mkdir /mnt/boot
+		mkdir /mnt/home
+		mount /dev/"$diskpart"2 /mnt/boot
+		mount /dev/"$diskpart"3 /mnt/home
+	elif [ $swap == "true" ] && [ $home == "false" ]; then
+		mount /dev/"$diskpart"4 /mnt
+		mkdir /mnt/boot
+		mount /dev/"$diskpart"2 /mnt/boot
+	else
+		mount /dev/"$diskpart"3 /mnt
+		mkdir /mnt/boot
+		mount /dev/"$diskpart"2 /mnt/boot
+	fi
+fi
+
+
+# The fun part begins, actually installing packages and the system
+
+"$dialog" --title "Installing packages" --msgbox "We will now install all required non aur packages, please stand by..." 8 78
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+pacstrap /mnt - < ./pacman.txt
+genfstab -U /mnt >> /mnt/etc/fstab
+
+
+# Chrooting time 
+
+"$dialog" --title "Chrooting into system" --msgbox "We will now chroot into your newly installed system to continue..." 8 78
+cp ./stage2.sh ./aur.txt /mnt/
+chmod +x /mnt/stage2.sh
+arch-chroot /mnt ./stage2.sh $disk
+rm -rf /mnt/stage2.sh /mnt/aur.txt
+umount -R /mnt
+reboot now
---
+++ stage2.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env /usr/bin/bash
+
+# Error code documentation
+#
+# 0 = Succesful exit
+# 1 = Whiptail and Dialog not install
+# 2 = User triggered exit
+
+# Setup for using extended globbing in bash
+
+shopt -s extglob
+
+# Global variables
+
+swap=false
+home=false
+ram=$(head /proc/meminfo | grep MemTotal: | awk '/^Mem/ {print $2}')
+ramGB=$(($ram/1000000))
+swapSize=0
+homeSize=0
+disk=""
+diskPart=""
+
+# Timezone variables
+
+continents=("Africa" "America" "Antarctica" "Asia" "Atlantic" "Australia" "Brazil" "Canada" "Chile" "Etc" "Europe" "Indian" " Mexico" "Pacific" "US")
+
+# aliases for printing colours quickly
+# rprintf -> prints to STDERR instead out STDOUT
+
+alias rprintf="printf '\\033[0;31m%s\\n''\\033[0;37m' >&2"
+alias gprintf="printf '\\033[0;32m%s\\n''\\033[0;37m'"
+alias yprintf="printf '\\033[0;33m%s\\n''\\033[0;37m'"
+alias bprintf="printf '\\033[0;34m%s''\\033[0;37m'"
+
+# Whiptail enviornment variables
+
+export NEWT_COLORS='
+root=,cyan
+window=,brightblue
+shadow=,black
+border=,brightblue
+title=black,brightblue
+textbox=black,brightblue
+button=brightblue,black
+compactbutton=black,brightblue
+'
+
+
+# Actual stage2 script
+
+# Thingy for swap space in VMs, thanks @lucie for this
+echo "vm.swappiness=1" >> /etc/sysctl.d/99-swappiness.conf
+
+# This is dirty but this is the only way I got it working, please don't judge me
+continent=$(whiptail --title "Timezone" --inputbox "Please give me the continent for your timezone\nCAPS MATTERS\n\n$(echo ${continents[@]})" 20 60 3>&1 1>&2 2>&3)
+city=$(whiptail --title "City" --inputbox "Please give me the city for your timezone\nCAPS MATTERS\n\n$(ls /usr/share/zoneinfo/$continent)" --scrolltext 20 60 3>&1 1>&2 2>&3)
+ln -sf /usr/share/zoneinfo/$continent/$city /etc/localtime
+
+hwclock --systohc
+
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+hostName=$(whiptail --title "Hostname" --inputbox "Please give me the name for this machine" 8 78 3>&1 1>&2 2>&3)
+
+echo "$hostName" >> /etc/hostname
+
+rootPasswd=$(whiptail --title "User password"  --passwordbox "Please input a password for the root account" 8 78 3>&1 1>&2 2>&3)
+
+echo "root:$rootPasswd" | chpasswd
+
+userName=$(whiptail --title "New user" --inputbox "Please give me the name for the new user you'd like to add" 8 78 3>&1 1>&2 2>&3)
+userPasswd=$(whiptail --title "User password"  --passwordbox "Please input a password for your newly created user" 8 78 3>&1 1>&2 2>&3)
+
+useradd --create-home "$userName"
+usermod -a -G wheel "$userName"
+chsh -s /bin/zsh "$userName"
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
+echo "$userName:$userPasswd" | chpasswd
+
+whiptail --title "Setup time" --msgbox "We will now setup all the packages, config files and even xorg for you, the computer will automatically reboot after this so make sure to pay attention incase it bootloops" 8 78
+
+sed -i 's/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
+sed -i 's/#ParallelDownloads = 5/ParallelDownloads = 5/g' /etc/pacman.conf
+
+# Downloading all necessary files
+runuser -l $userName -c "git clone https://git.codingvm.codes/emily/dotfiles && git clone https://aur.archlinux.org/yay && git clone https://git.liveemily.xyz/Emily/dwm-pkgbuild.git && git clone https://github.com/LukeSmithxyz/st && curl https://liveemily.xyz/archieinstall/wallpaper.png --output ~/wallpaper.png"
+# Setup config files
+runuser -l $userName -c "mkdir ~/.config && mv dotfiles/nvim dotfiles/zsh dotfiles/shell ~/.config/ && mv dotfiles/.vimrc ~/ && touch ~/.zshrc && echo 'source ~/.config/zsh/.zshrc' >> ~/.zshrc"
+# Install yay and all aur packages
+runuser -l $userName -c "cd yay && makepkg -si --noconfirm && cd .. && yay -S - < aur.txt"
+# Install window manager
+runuser -l $userName -c "cd dwm-pkgbuild && makepkg -si --noconfirm --skipchecksums && touch ~/.xinitrc && echo '/etc/dwm/autostart' >> ~/.xinitrc && cd ../st && sudo make install"
+
+# Cleanup time
+runuser -l $userName -c "pacman -Scc && yay -Scc && rm -rf ~/dotfiles && rm -rf ~/yay && rm -rf ~/dwm-pkgbuild && rm -rf ~/st && rm -rf ~/.cache/* && rm -rf /tmp/*"
+sed -i 's/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
+
+disk=$1
+
+if [[ -d "/sys/firmware/efi" ]]; then
+	grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+else
+	grub-install --target=i386-pc /dev/"$disk"
+fi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+whiptail --title "Finished!" --msgbox "Phew that was quite some time, but everything should be finished now. Your computer will now reboot and you should be able to login with your username and password. Afterwards please type in the command 'startx' and press enter to get into your new system!" 8 78
+
+exit 0
